c7d377dec17288056f7c1e17b2561200742538ff
anna_umi <anna.a.vlasenko@yandex.ua>
2019-03-17 19:05:46 +0200

    https redirect

--- server.js
+++ server.js
@@ -1,76 +1,80 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+var http = require("http");
+var enforce = require("express-sslify");
 
-require('dotenv').config({path: 'variables.env'});
+require("dotenv").config({ path: "variables.env" });
 
-const Recipe = require('./models/Recipe');
-const User = require('./models/User');
+const Recipe = require("./models/Recipe");
+const User = require("./models/User");
 
-const {graphiqlExpress, graphqlExpress} = require('apollo-server-express');
-const {makeExecutableSchema} = require('graphql-tools')
+const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
 
-const {typeDefs} = require('./schema');
-const {resolvers} = require('./resolvers');
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
 
 const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers
+});
 
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log('DB connected'))
-    .catch(err => console.error(err));
-
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
 
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true
-}
+  origin: "http://localhost:3000",
+  credentials: true
+};
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors(corsOptions));
 
 //Set up JWT authentification middleware
-app.use(async (req, res, next)=> {
-    const token = req.headers['authorization'];
-    if(token !== "null"){
-        try{
-            const currentUser = await jwt.verify(token, process.env.SECRET);
-            req.currentUser = currentUser;
-        }catch(err){
-            console.log(err)
-        }
+app.use(async (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (token !== "null") {
+    try {
+      const currentUser = await jwt.verify(token, process.env.SECRET);
+      req.currentUser = currentUser;
+    } catch (err) {
+      console.log(err);
     }
-    next();
-})
+  }
+  next();
+});
 
 //app.use('/graphiql', graphiqlExpress({
-  //  endpointURL: '/graphql'
+//  endpointURL: '/graphql'
 //}));
 
-app.use('/graphql', 
-    bodyParser.json(),
-    graphqlExpress(({currentUser})=>({
+app.use(
+  "/graphql",
+  bodyParser.json(),
+  graphqlExpress(({ currentUser }) => ({
     schema,
     context: {
-        Recipe,
-        User,
-        currentUser
+      Recipe,
+      User,
+      currentUser
     }
-})));
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+  }))
+);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}`)
+  console.log(`Server listening on PORT ${PORT}`);
 });
